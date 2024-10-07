c61f9e091be9552552de47f4f6590d1f6b6d2c44
skyler dennis <skylerdenniss11@gmail.com>
2024-10-07 17:58:22 -0400

    Finnished part one of selection statements

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿}
+using Internal;
+
+namespace SelectionStatementExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var favNumber = 555;
+            int userInput;
+
+            do
+            {
+
+                Console.WriteLine("Try and guess the number from 1 to 1000");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput > favNumber)
+                {
+                    Console.WriteLine("nope, Too high!");
+
+                }
+
+                else if (userInput < favNumber)
+                {
+                    Console.WriteLine("Almost, But to low!");
+                }
+
+                else
+                {
+                    Console.WriteLine("CORRECT!!!");
+                }
+
+
+            } while (userInput != favNumber);
+
+        }
+
+    }
+}
