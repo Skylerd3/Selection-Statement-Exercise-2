b6007f8855ed91b4a38468abb754b21c7dc64476
skyler dennis <skylerdenniss11@gmail.com>
2024-10-16 10:20:19 -0400

    Madee the swith statement not case sensative

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -10,25 +10,25 @@
 
             switch (favSubject)
             {
-                case "math":
-                    Console.WriteLine("I have allways liked Math because its all about solving problems!");
+                case "Math" or "math":
+                    Console.WriteLine("Good choice, I have allways liked Math because its all about solving problems!");
                     break;
 
-                case "sciance":
-                    Console.WriteLine("I love sciance because it explanse the whole world!");
+                case "Sciance" or "sciance":
+                    Console.WriteLine("Good choice, I love sciance because it explanse the whole world!");
                     break;
 
 
-                case "reading":
-                    Console.WriteLine("Reading has never been my strongest but I love to learn from it!");
+                case "Reading" or "reading":
+                    Console.WriteLine("Good choice, Reading has never been my strongest.");
                     break;
 
-                case "PE":
-                    Console.WriteLine("PE has allways been one of my favorits!");
+                case "PE" or "Pe" or "pe":
+                    Console.WriteLine("Good choice, PE has allways been one of my favorits!");
                     break;
 
-                case "writing":
-                    Console.WriteLine("Writing was allways hard because Im horible at spelling!");
+                case "Writing" or "writing":
+                    Console.WriteLine("Good choice, Writing was allways hard because Im horible at spelling!");
                     break;
 
                 default:
